b64d219c5bd94023d9d303bd987341b497cc5d91
kengoon <kengoon19@gmail.com>
2021-01-13 00:57:05 +0100

    too lazy to write

--- libs/libpy/settings.py
+++ libs/libpy/settings.py
@@ -9,11 +9,11 @@ class Setting(Screen):
     @staticmethod
     def add_all_rules(rule1, rule2):
         def loop_add_rule(rules):
-            for rule in rules.items:
+            for index, rule in enumerate(rules.items):
                 rules.add_widget(
                     MDExpansionPanel(
                         icon=rules.items[rule],
-                        # content=content,
+                        content=rules.content[index],
                         panel_cls=MDExpansionPanelOneLine(text=rule),
                     )
                 )
